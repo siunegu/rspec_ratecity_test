34b8f708acd342fe0f95e8902f24ea9c06bc6e81
Eugenius Hei Man Lai <equalsbrackets@gmail.com>
2015-03-12 16:43:50 +1100

    5 examples 0 failures

--- lib/page.rb
+++ lib/page.rb
@@ -1,39 +1,21 @@
 require 'pry'
+require 'json'
 
 Page = Struct.new(:name, :content) do 
 	def self.from_map(map)
 		# implement from_map to pass the specifications found in page_spec.rb
 		map_name = map['name'] 
 		map_content = map['content'] 
-
-		b = self.new(map_name, map_content)
-		puts b
+		self.new(map_name, map_content)
 	end
 
-	def self.ingest(json)		
-		json.each do |x|
-			a = self.new(x[:name], x[:content])
-			puts a
-			# binding.pry
+	def self.ingest(json)						
+		json_object = JSON.parse(json)
+		array = []
+		json_object.each do |x|		
+
+			array << self.new(x['name'], x['content']) 			
 		end
-		# json.each do |json_record|
-		# 	json_record
-		# 	record_name = json_record[:name]
-		# 	record_content = json_record[:content]			
-		# 	a = self.new(record_name, record_content)
-		# 	binding.pry
-		# end
+		array
 	end
 end
-
-json =  [
-          {"name": "one", "content": "one content"},
-          {"name": "two", "content": "two content"}
-        ]
-Page.ingest(json)
-
-
-map = {'name' => 'Name',
-       'content' => 'The content.'
-      }
-Page.from_map(map)
